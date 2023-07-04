a8f14bbec67b596a7b84d6e89b49d596f0a8e827
Constantine Chernevsky <97674120+nehardcore@users.noreply.github.com>
2023-07-03 19:12:30 -0700

    first task done

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+module "test-vm" {
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name        = "develop"
+  network_id      = yandex_vpc_network.develop.id
+  subnet_zones    = ["ru-central1-a"]
+  subnet_ids      = [ yandex_vpc_subnet.develop.id ]
+  instance_name   = "web"
+  instance_count  = 1
+  image_family    = "ubuntu-2004-lts"
+  public_ip       = true
+  
+  metadata = {
+      user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+      serial-port-enable = 1
+  }
+
+}
+
+data "template_file" "cloudinit" {
+  vars = {
+    vms_ssh_root_key = var.vms_ssh_root_key
+  }
+  template = file("./cloud-init.yml")
+}
---
+++ personal.auto.tfvars
@@ -0,0 +1,5 @@
+token  =  "y0_AgAAAAAbot-qAATuwQAAAADaqpxzyGFlHRqNQJSTyFbt5IK0IRnkNHk"
+cloud_id  = "b1gc2ivfgbfpeae51hfs"
+folder_id = "b1gbcq5a26p0lc4ug961"
+
+vms_ssh_root_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKq8dxK9r+Ca87zUgTgWGlaGMr9sQDNQ20iYAxwPbiNB cch@MBP-Costas"
