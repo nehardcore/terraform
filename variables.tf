de7c956497ea4af1e81ad9f1926ad56879cccf58
Constantine Chernevsky <97674120+nehardcore@users.noreply.github.com>
2023-07-18 17:01:19 -0700

    hw05-done

--- variables.tf
+++ variables.tf
@@ -51,3 +51,23 @@ variable "vms_ssh_root_key" {
 #   default     = "netology-develop-platform-db"
 #   description = "example vm_db_ prefix"
 # }
+
+variable "vm_local_ip" {
+  type = string
+  default = "10.0.1.4"
+  validation {
+    condition     = can(cidrnetmask("${var.vm_local_ip}/32"))
+    error_message = "Must be a valid IPv4 CIDR block address."
+  }
+}
+
+# variable "vm_local_ips" {
+#   type = list(string)
+#   default = ["10.0.1.4", "192.168.0.4", "127.0.0.1"]
+#   validation {
+#     condition = alltrue([
+#       for a in var.vm_local_ips : can(cidrnetmask("${a}/32"))
+#     ])
+#     error_message = "All elements must be valid IPv4 CIDR block addresses."
+#   }
+# }
